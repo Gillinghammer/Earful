f4f72d36b5399e6d3eb2357331881a789de44377
Gillinghammer <colingillingham@gmail.com>
2014-10-10 11:19:19 +0100

    added devise views and omniauth for FB

---
+++ earful/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :user_name
+  # attr_accessible :title, :body
+end
