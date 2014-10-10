95fd7b80d212910b632721e687e29b395d631d12
Gillinghammer <colingillingham@gmail.com>
2014-10-10 10:43:55 +0100

    rails app and folders

---
+++ earful/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Earful::Application.config.secret_token = 'dcba23ea584fbe1a4e32ac5dd59a8d358ec89f4764d794f0efe0ea6df98ecaf52819c095357758e607eb073d572edc13af058dbf288c0daf503daa9866a0de0c'
