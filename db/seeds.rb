aeb73fe7cf55d2171cd19cb730c64f32dbe06c67
Gillinghammer <colingillingham@gmail.com>
2014-10-10 15:23:09 +0100

    added user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(user_name: "Colin Gillingham", email: "colingillingham@gmail.com")
