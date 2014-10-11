2be4114738f7681a999e03d0df76efe2e73f498b
Gillinghammer <colingillingham@gmail.com>
2014-10-11 22:39:47 +0100

    updated processor to try to fix bug

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,13 +1,23 @@
 class EmailProcessor
+  
   def self.process(email)
-    # Post.create!({ content: email.body, title: email.subject,  })
-        user = User.find_by_email(@email.from[:email])
-        user.posts.create!(
-          title: @email.subject,
-          content: @email.body,
-          public: true,
-          sound: @email.attachments[0]
-        )
-
+      Post.create!({ title: "this is a test" content: email.body })
   end
+
+
+  # def self.process(email)
+  #   # Post.create!({ content: email.body, title: email.subject,  })
+  #     puts
+  #     puts "----!!!---"
+  #     puts "processing new email now..."
+  #     puts "----!!!---"
+  #     puts
+  #       user = User.find_by_email(@email.from[:email])
+  #       user.posts.create!(
+  #         title: @email.subject,
+  #         content: @email.body,
+  #         # public: true,
+  #         # sound: @email.attachments[0]
+  #       )
+  # end
 end
