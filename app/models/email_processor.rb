acea4ceeea53deb0d6e2ae27142f8102758adb30
Gillinghammer <colingillingham@gmail.com>
2014-10-11 23:21:39 +0100

    removed config options

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,23 +1,17 @@
-class EmailProcessor < ActiveRecord::Base
-  
-  def self.process(email)
-      Post.create!({ title: "this is a test", content: email.body })
+class EmailProcessor
+  def initialize(email)
+    @email = email
   end
 
+  def process
+    # all of your application-specific code here - creating models,
+    # processing reports, etc
 
-  # def self.process(email)
-  #   # Post.create!({ content: email.body, title: email.subject,  })
-  #     puts
-  #     puts "----!!!---"
-  #     puts "processing new email now..."
-  #     puts "----!!!---"
-  #     puts
-  #       user = User.find_by_email(@email.from[:email])
-  #       user.posts.create!(
-  #         title: @email.subject,
-  #         content: @email.body,
-  #         # public: true,
-  #         # sound: @email.attachments[0]
-  #       )
-  # end
+    # here's an example of model creation
+    user = User.find_by_email(@email.from[:email])
+    user.posts.create!(
+      title: @email.subject,
+      content: @email.body
+    )
+  end
 end
