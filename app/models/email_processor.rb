cdf2659f40dc6286178bc3a7990a80f8fae1cdbe
Gillinghammer <colingillingham@gmail.com>
2014-10-12 18:39:59 +0100

    troubleshooting carrierwav

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,11 +5,11 @@ class EmailProcessor
 
   def process
 
-    puts @email.attachments[0]
+    puts @email.attachments.inspect
     # all of your application-specific code here - creating models,
     # processing reports, etc
     new_post = Post.create!(title: @email.subject, content: @email.body)
-    new_post.sound = @email.attachments[0]
+    # new_post.sound = @email.attachments[0]
     # here's an example of model creation
     user = User.find_by_email(@email.from[:email])
     user.posts << new_post
