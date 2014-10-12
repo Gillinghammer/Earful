020aa4691f97fe682504d586be28eb9f9994955b
Gillinghammer <colingillingham@gmail.com>
2014-10-12 20:17:48 +0100

    updated so that posts get created even if no user is attached, fornow

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,14 +4,12 @@ class EmailProcessor
   end
 
   def process
-
-    
     # all of your application-specific code here - creating models,
     # processing reports, etc
     new_post = Post.create!(title: @email.subject, content: @email.body)
     new_post.sound = @email.attachments[0]
     # here's an example of model creation
     user = User.find_by_email(@email.from[:email])
-    user.posts << new_post
+    user.posts << new_post if user
   end
 end
