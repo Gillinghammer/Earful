393175aa0431247785bd97392f7e2dbb9f358258
Gillinghammer <colingillingham@gmail.com>
2014-10-12 17:30:34 +0100

    working out validation bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_attached_file :sound
   # validates_attachment_content_type :sound, :content_type => ['audio/mp4']
   #validates_attachment :sound_content_type, content_type: {content_type: ['audio/mp4']}
-  validates_attachment :sound, :presence => true,
-    :content_type => { :content_type => "audio/mp4" }
+  # validates_attachment :sound, :presence => true,
+    # :content_type => { :content_type => "audio/mp4" }
+  validates_attachment_content_type :sound, :content_type => ['audio/mp4', 'audio/mp3', 'application/x-mp4']
 end
