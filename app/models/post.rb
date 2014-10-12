d8c5a9954c45858bd9e36f5cdb0dd3cf2e3eb3df
Gillinghammer <colingillingham@gmail.com>
2014-10-12 17:09:39 +0100

    commented out the validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :content, :public, :sound, :tags, :title, :sound_file_name
   has_attached_file :sound
-  #validates_attachment :sound, content_type: {content_type: ['audio/mp4']}
+  validates_attachment_content_type :sound, :content_type => ['audio/mp4']
+  #validates_attachment :sound_content_type, content_type: {content_type: ['audio/mp4']}
 end
