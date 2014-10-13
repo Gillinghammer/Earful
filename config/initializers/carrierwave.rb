21862fcc68d58854f4af8fb8f3602796b4cccbcb
Gillinghammer <colingillingham@gmail.com>
2014-10-13 12:06:29 +0100

    created temporary uploads folder

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,7 @@
 CarrierWave.configure do |config|
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+
   config.fog_credentials = {
     :provider => 'AWS',                        # required
     :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],                        # required
