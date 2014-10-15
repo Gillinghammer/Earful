0749541d8806d77b278fe73e96198b3f25a73a2a
Gillinghammer <colingillingham@gmail.com>
2014-10-15 16:53:32 +0100

    fixed a bug

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,6 +7,6 @@ class EmailProcessor
     new_post = Post.create!(title: @email.subject, content: @email.body)
     new_post.sound = @email.attachments[0]
     user = User.find_by_email(@email.from[:email])
-    user.posts << new_post  if user.persisted?
+    user.posts << new_post  if user
   end
 end
