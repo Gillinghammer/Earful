e98c5b8532426a058ac6699e076e6f8a5435c657
Gillinghammer <colingillingham@gmail.com>
2014-10-22 11:17:48 +0100

    added fog

--- app/uploaders/sound_uploader.rb
+++ app/uploaders/sound_uploader.rb
@@ -2,8 +2,8 @@
 
 class SoundUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  #storage :fog
+  #storage :file
+  storage :fog
 
   def store_dir
     # "earful_uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
 
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  #config.cache_dir = "#{Rails.root}/tmp/uploads"
 
   config.fog_credentials = {
     :provider => 'AWS',                        # required
