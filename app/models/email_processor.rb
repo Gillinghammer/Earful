e70fd4f4b94584c182c1fff250c40df170f4d5e3
Gillinghammer <colingillingham@gmail.com>
2014-10-22 14:08:59 +0100

    added square request from google image

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,10 +6,10 @@ class EmailProcessor
   def process
     user = User.find_by_email(@email.from[:email])
     if user
-    new_post = Post.create!(title: @email.subject, content: @email.body, public: true)
-    new_post.sound = @email.attachments[0]
+      new_post = Post.create!(title: @email.subject, content: @email.body, public: true)
+      new_post.sound = @email.attachments[0]
     
-    user.posts << new_post  if user
-  end
+      user.posts << new_post  if user
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.user_name = auth.info.name
-      user.image = auth.info.image 
+      user.image = auth.info.image
     end
   end
   def self.new_with_session(params, session)
