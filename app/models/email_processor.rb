058a52af367e000cc284da2e43679c2e66c764e6
Gillinghammer <colingillingham@gmail.com>
2014-10-22 15:17:55 +0100

    condition applied to prevent duplicate sounds from uploading

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -9,7 +9,9 @@ class EmailProcessor
       new_post = Post.create!(title: @email.subject, content: @email.body, public: true)
       new_post.sound = @email.attachments[0]
     
-      unless Post.find_by_title(new_post.title) && Post.find_by_user_id(new_post.user_id)
+      if Post.find_by_title(new_post.title) && Post.find_by_user_id(user.id)
+        puts "Alert! this file has already been uploaded"
+      else 
         user.posts << new_post
       end
     end
