953da5044f878c008c69a0fed79184a230989c7d
Gillinghammer <colingillingham@gmail.com>
2014-10-22 15:00:31 +0100

    condition applied to prevent duplicate sounds from uploading

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -9,8 +9,9 @@ class EmailProcessor
       new_post = Post.create!(title: @email.subject, content: @email.body, public: true)
       new_post.sound = @email.attachments[0]
     
-
-      user.posts << new_post  unless Post.find_by_sound(new_post.sound) && Post.find_by_user_id(user.id)
+      unless Post.find_by_sound(new_post.sound) && Post.find_by_user_id(new_post.user_id)
+        user.posts << new_post
+      end
     end
   end
 end
