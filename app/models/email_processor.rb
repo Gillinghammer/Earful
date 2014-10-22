ca0e4d5bc42b68fe9a1d2323189bf117ad10585b
Gillinghammer <colingillingham@gmail.com>
2014-10-22 15:25:19 +0100

    fixed email processor to avoid duplication

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,12 +6,11 @@ class EmailProcessor
   def process
     user = User.find_by_email(@email.from[:email])
     if user
-      new_post = Post.create!(title: @email.subject, content: @email.body, public: true)
-      new_post.sound = @email.attachments[0]
-    
-      if Post.find_by_title(new_post.title) && Post.find_by_user_id(user.id)
+      if Post.find_by_title(@email.subject) && Post.find_by_user_id(user.id)
         puts "Alert! this file has already been uploaded"
       else 
+        new_post = Post.create!(title: @email.subject, content: @email.body, public: true)
+        new_post.sound = @email.attachments[0]
         user.posts << new_post
       end
     end
