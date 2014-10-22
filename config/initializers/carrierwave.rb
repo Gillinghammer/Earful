a085fa20b9677d88eff23083a8afe360e9e6e996
Gillinghammer <colingillingham@gmail.com>
2014-10-22 11:35:26 +0100

    added Fog.credentials = { path_style: true } to application.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], # required
     :region => 'Ireland'
-    :path_style => true # optional, defaults to 'us-east-1'
+   # optional, defaults to 'us-east-1'
     #:host                   => 's3.example.com',             # optional, defaults to nil
     #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
