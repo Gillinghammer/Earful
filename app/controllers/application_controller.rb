0509d76e4b579a4a44898af58a472212dfaede06
Gillinghammer <colingillingham@gmail.com>
2014-10-22 13:56:52 +0100

    sfs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,17 @@ class ApplicationController < ActionController::Base
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Request-Method'] = '*'
     end
+    
+      after_filter :set_csrf_cookie_for_ng
+
+      protected
+      def set_csrf_cookie_for_ng
+        cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+      end
+
+      protected
+      def verified_request?
+        super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+      end
 
 end
